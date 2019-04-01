8d99719d5e9a572520d255172aa97a9a98257bb3
charles-desouza <charles5388@gmail.com>
2019-04-01 17:09:36 +0530

    Adding options

--- day1/react-training/my-app/src/components/addOption.js
+++ day1/react-training/my-app/src/components/addOption.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react'
 
 
 export default class AddOption extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    }
+  
+  handleAddOption(e){
+    e.preventDefault();
+
+    const mydata = e.target.elements.option.value.trim();
+    this.props.handleAddOption(mydata);
+  }
+
+
   render() {
     return (
       <div>
-        AddOption component here
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add</button>
+        </form>
       </div>
     );
   }
--- day1/react-training/my-app/src/components/indecisionApp.js
+++ day1/react-training/my-app/src/components/indecisionApp.js
@@ -10,9 +10,10 @@ export default class IndecisionApp extends Component {
   constructor(props) {
     super(props);
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
 
     this.state = {
-      options: ["One", "Two", "Three"]
+      options: []
     }
   }
 
@@ -36,6 +37,20 @@ export default class IndecisionApp extends Component {
     })
   }
 
+  handleAddOption(option) {
+
+    if(!option){
+      return 'Enter valid value to add item'
+    } else if( this.state.options.indexOf(option) > -1){
+      return 'This option already exists'
+    }
+    this.setState((prevState) => {
+      return {
+        options: prevState.options.concat(option)
+      };
+    });
+  }
+
   render() {
 
     const headerTitle = "Indecision";
@@ -46,7 +61,7 @@ export default class IndecisionApp extends Component {
         <Header title={headerTitle} />
         <Action hasOptions={this.state.options.length > 0} />
         <Options options={this.state.options} handleDeleteOptions={this.handleDeleteOptions} />
-        <AddOption />
+        <AddOption handleAddOption = {this.handleAddOption}/>
         <Footer title={footerTitle} />
       </div>
     );
